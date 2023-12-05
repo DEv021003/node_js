b7eda1660e2d0229caf46fec67540d3fe18c32e8
Luu Quang Tu <sam@google.com>
2023-12-05 09:57:42 +0700

    su dung dotenv

--- src/server.js
+++ src/server.js
@@ -1,23 +1,28 @@
+require('dotenv').config();
 const express = require('express');
-
+const path = require('path');
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 8888;//port => hardcode
+const hostname = process.env.HOST_NAME;
 
-app.set('views', './src/views');
-app.set('view engine', 'ejs')
+// console.log(">>> check:" , process.env);
+
+// app.set('views', './src/views/');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 	res.send('hello	')
-})
+});
 app.get('/1', (req, res) => {
     res.send('check 1')
-})
+});
 
 app.get('/2', (req, res) => {
     // res.send('<h1>Check 1</h1>')
     res.render('sample.ejs')
-})
+});
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
