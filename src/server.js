438a2b85272da2d081d6df1d9b5d481f191960c0
Luu Quang Tu <sam@google.com>
2023-12-05 12:38:20 +0700

    mvc

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+
+const mysql = require('mysql2');
+
 const app = express();
 const port = process.env.PORT || 8888;//port => hardcode
 const hostname = process.env.HOST_NAME;
@@ -11,6 +14,24 @@ configViewEngine(app);
 
 //khai bao route
 app.use('/', webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',// default : empty
+    password: '123456',
+    database: 'tudeptrai',
+    port: 3307 //default 3306
+});
+
+//simple query
+connection.query(
+    'SELECT * FROM Users u ;',
+    function(err, results, fields){
+        console.log(results);
+        console.log(fields);
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
