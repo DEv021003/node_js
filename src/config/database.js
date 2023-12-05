477923f00d68121d2c8dd496ff917b35d1888bf2
Luu Quang Tu <sam@google.com>
2023-12-05 13:24:34 +0700

    su dung connectionPool

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,// default : empty
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     port: process.env.DB_PORT //default 3306
+// });
+
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,// default : empty
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT //default 3306
+    port: process.env.DB_PORT, //default 3306
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
+
 
 module.exports = connection;
---
+++ src/config/old.js
@@ -0,0 +1,13 @@
+//test code
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,// default : empty
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT //default 3306
+});
+
+module.exports = connection;
