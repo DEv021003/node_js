3a70e49545771ed36aa9eb9726b1cbedbf9d26c6
Luu Quang Tu <sam@google.com>
2023-12-05 14:48:50 +0700

    tao create user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,23 @@
 const connection = require('../config/database');
+const { post } = require('../routes/web');
 
 const getHomepage = (req, res) => {
     return res.render('home.ejs')
 }
 
-const getAbout = (req, res) => {
-    res.send('haha')
+const getCreate = (req, res) => {
+    res.render('sample.ejs')
 }
 
 const getAnh = (req, res) => {
     res.render('sample.ejs')
 }
 
+const postCreateUser = (req, res) => {
+    console.log(">>> req.body: ", req.body)
+    res.send('create a new user')
+}
+
 module.exports = {
-    getHomepage, getAnh, getAbout
+    getHomepage, getAnh, getCreate,postCreateUser
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const {getHomepage,getAnh,getAbout} = require('../controllers/homeController');
+const {getHomepage,getAnh,getCreate,postCreateUser} = require('../controllers/homeController');
 const router = express.Router();
 
 // router.Method('/route', handler)
 
 router.get('/', getHomepage);
-router.get('/1', getAbout);
+router.get('/1', getCreate);
 router.get('/2', getAnh);
 
-module.exports = router;
+router.post('/create-user', postCreateUser);
+
+module.exports = router; //export default
